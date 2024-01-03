38854c6a85e0ad96fa38f8f4239b17375fcb745f
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2024-01-03 23:30:54 +0530

    Update keyboard header file

--- include/keyboard.h
+++ include/keyboard.h
@@ -6,6 +6,10 @@
 typedef struct {
     uint8_t keymap[87];
     uint8_t keypressed[17];
-};
+    void (* onNextKeyPress) (keyboard_t* keyboard, uint8_t);
+} keyboard_t;
+
+void initKeyBoard(keyboard_t *keyboard);
+void onNextKeyPress(keyboard_t *keyboard, uint8_t);
 
 #endif
